95dac7b66386fc21e48a3647a0eb0a582f4c07a8
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-07-28 10:07:56 -0400

    created repo

---
+++ p2pkh.js
@@ -0,0 +1,42 @@
+import { URL } from 'url';
+import { compileFile } from 'cashc';
+import { ElectrumNetworkProvider, Contract, SignatureTemplate } from 'cashscript';
+import { stringify } from '@bitauth/libauth';
+
+// Import Alice's keys from common-js.js
+import { alicePkh, alicePriv, alicePub } from './common-js.js';
+
+// Compile the P2PKH contract to an artifact object
+const artifact = compileFile(new URL('p2pkh.cash', import.meta.url));
+
+// Initialise a network provider for network operations on CHIPNET
+const provider = new ElectrumNetworkProvider('chipnet');
+
+// Instantiate a new contract using the compiled artifact and network provider
+// AND providing the constructor parameters (pkh: alicePkh)
+const contract = new Contract(artifact, [alicePkh], { provider });
+
+// Get contract balance & output address + balance
+console.log('contract address:', contract.address);
+console.log('contract balance:', await contract.getBalance());
+
+// Call the spend function with alice's signature + pk
+// And use it to send 0. 000 100 00 BCH back to the contract's address
+
+
+const tx = await contract.functions
+  .spend(alicePub, new SignatureTemplate(alicePriv))
+  .to(contract.address, 10000n)
+  .send();
+
+console.log('transaction details:', stringify(tx));
+
+// // Call the spend function with alice's signature + pk
+// // And use it to send two outputs of 0. 000 150 00 BCH back to the contract's address
+// const tx2 = await contract.functions
+//   .spend(alicePub, new SignatureTemplate(alicePriv))
+//   .to(contract.address, 15000n)
+//   .to(contract.address, 15000n)
+//   .send();
+
+//console.log('transaction details:', stringify(tx2));
